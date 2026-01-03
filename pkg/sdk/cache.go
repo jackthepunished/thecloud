94760834f41c7f8601a4f2b8c67762e67973b573
PoyrazK <hpk.poyraz@gmail.com>
2026-01-03 13:50:14 +0300

    feat: implement cache sdk

---
+++ pkg/sdk/cache.go
@@ -0,0 +1,87 @@
+package sdk
+
+import "time"
+
+type Cache struct {
+	ID          string    `json:"id"`
+	UserID      string    `json:"user_id"`
+	Name        string    `json:"name"`
+	Engine      string    `json:"engine"`
+	Version     string    `json:"version"`
+	Status      string    `json:"status"`
+	VpcID       *string   `json:"vpc_id,omitempty"`
+	ContainerID string    `json:"container_id,omitempty"`
+	Port        int       `json:"port"`
+	Password    string    `json:"password,omitempty"` // Only returned on Create/Get usually?
+	MemoryMB    int       `json:"memory_mb"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type CreateCacheInput struct {
+	Name     string  `json:"name"`
+	Version  string  `json:"version"`
+	MemoryMB int     `json:"memory_mb"`
+	VpcID    *string `json:"vpc_id,omitempty"`
+}
+
+type CacheStats struct {
+	UsedMemoryBytes  int64 `json:"used_memory_bytes"`
+	MaxMemoryBytes   int64 `json:"max_memory_bytes"`
+	ConnectedClients int   `json:"connected_clients"`
+	TotalKeys        int64 `json:"total_keys"`
+}
+
+func (c *Client) CreateCache(name, version string, memoryMB int, vpcID *string) (*Cache, error) {
+	input := CreateCacheInput{
+		Name:     name,
+		Version:  version,
+		MemoryMB: memoryMB,
+		VpcID:    vpcID,
+	}
+	var resp Response[Cache]
+	if err := c.post("/caches", input, &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Data, nil
+}
+
+func (c *Client) ListCaches() ([]*Cache, error) {
+	var resp Response[[]*Cache]
+	if err := c.get("/caches", &resp); err != nil {
+		return nil, err
+	}
+	return resp.Data, nil
+}
+
+func (c *Client) GetCache(id string) (*Cache, error) {
+	var resp Response[Cache]
+	if err := c.get("/caches/"+id, &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Data, nil
+}
+
+func (c *Client) DeleteCache(id string) error {
+	return c.delete("/caches/"+id, nil)
+}
+
+func (c *Client) GetCacheConnectionString(id string) (string, error) {
+	var resp Response[map[string]string]
+	if err := c.get("/caches/"+id+"/connection", &resp); err != nil {
+		return "", err
+	}
+	return resp.Data["connection_string"], nil
+}
+
+func (c *Client) FlushCache(id string) error {
+	return c.post("/caches/"+id+"/flush", nil, nil)
+}
+
+func (c *Client) GetCacheStats(id string) (*CacheStats, error) {
+	var resp Response[CacheStats]
+	if err := c.get("/caches/"+id+"/stats", &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Data, nil
+}
